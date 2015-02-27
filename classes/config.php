8910ee867d7c7a1ac00a44ca46a859c8dba9157b
Marcus Hambraeus <marcus.hambraeus@gmail.com>
2015-02-27 10:55:32 +0100

    Error in Config-file

--- classes/config.php
+++ classes/config.php
@@ -4,18 +4,18 @@
 		protected $config = array();
 
 		public function __construct($file = 'config.ini') {
-			$this->config = parse_ini_file(dirname(dirname(__FILE__)) . $file);
+			$this->config = parse_ini_file(dirname(dirname(__FILE__)) . '/' . $file, true);
 		}
 
 		public function Get($Domain, $Field) {
 			if($this->Has($Domain, $Field)) {
-				return $config[$Domain][$Field];
+				return $this->config[$Domain][$Field];
 			}
 			throw new Exception('Undefined variable ' . $Domain . ':' . $Field);
 		}
 
 		public function Has($Domain, $Field) {
-			return isset($config[$Domain][$Field]);
+			return isset($this->config[$Domain][$Field]);
 		}
 	}
 
