5ea89f370c7612dda5579b346326aa8ca5896017
Marcus Hambraeus <marcus.hambraeus@gmail.com>
2015-02-27 11:07:38 +0100

    Better redirect-logic in auth.php

--- webroot/auth.php
+++ webroot/auth.php
@@ -37,6 +37,10 @@
 		}
 	}
 
-	header('Location: index.php');
+	if(isset($_SERVER['HTTP_REFERER'])) {
+		header('Location: ' . $_SERVER['HTTP_REFERER']);
+	} else {
+		header('Location: /index.php');
+	}
 
 ?>
